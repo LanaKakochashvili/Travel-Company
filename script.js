0af30a7de76c0da9fe4ea25c653c1038283a6fc8
LanaKakochashvili <122685695+LanaKakochashvili@users.noreply.github.com>
2023-02-22 23:56:21 +0400

    done

---
+++ script.js
@@ -0,0 +1,4 @@
+let navigationElement = document.getElementById ("navBar");
+let burgerElement = document.getElementById ("burgerBar"):
+
+burgerElement.addEventListener("click", function (){navigationElement.classList.toggle("activeNav");});
